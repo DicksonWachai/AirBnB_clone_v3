724a90bfddbd14b0febb98b7fa7c67ff1f75ecfe
DicksonWachai <wachaidickson@gmail.com>
2024-09-04 08:14:22 +0000

    Update code to retrieve all state objects

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""All default RESTFUL API actions for States"""
+"""Handles all RESTful API actions for State"""
 
 from api.v1.views import app_views
 from flask import request, jsonify, abort
@@ -8,51 +8,52 @@ from models.state import State
 from models import storage
 
 
-@app_views.route('/states',
-                 methods=['GET', 'POST'], strict_slashes=False)
-@app_views.route('/states/<state_id>',
-                 methods=['GET', 'PUT', 'DELETE'], strict_slashes=False)
+@app_views.route('/states', methods=['GET', 'POST'], strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['GET', 'PUT', 'DELETE'], strict_slashes=False)
 def states(state_id=None):
-    """Retrieves a list of state objects"""
-
-    state_objs = storage.all(State)
-
-    states = [obj.to_dict() for obj in state_objs.values()]
-    if not state_id:
+    """Handles state objects based on HTTP methods"""
+    if state_id is None:
         if request.method == 'GET':
+            """Retrieve all state objects"""
+            states = [state.to_dict() for state in storage.all(State).values()]
             return jsonify(states)
-        elif request.method == 'POST':
-            my_dict = request.get_json()
 
-            if my_dict is None:
+        elif request.method == 'POST':
+            """Create a new State object"""
+            my_dict = request.get_json()
+            if not my_dict:
                 abort(400, 'Not a JSON')
-            if my_dict.get("name") is None:
+            if 'name' not in my_dict:
                 abort(400, 'Missing name')
+
             new_state = State(**my_dict)
             new_state.save()
             return jsonify(new_state.to_dict()), 201
+    
     else:
-        if request.method == 'GET':
-            for state in states:
-                if state.get('id') == state_id:
-                    return jsonify(state)
+        state = storage.get(State, state_id)
+        if not state:
             abort(404)
-        elif request.method == 'PUT':
-            my_dict = request.get_json()
 
-            if my_dict is None:
+        if request.method == 'GET':
+            """Retrieve a specific State object"""
+            return jsonify(state.to_dict())
+
+        elif request.method == 'PUT':
+            """Update a State object"""
+            my_dict = request.get_json()
+            if not my_dict:
                 abort(400, 'Not a JSON')
-            for state in state_objs.values():
-                if state.id == state_id:
-                    state.name = my_dict.get("name")
-                    state.save()
-                    return jsonify(state.to_dict()), 200
-            abort(404)
+            
+            """Update fields in the State object"""
+            for key, value in my_dict.items():
+                if key not in ['id', 'created_at', 'updated_at']:
+                    setattr(state, key, value)
+            state.save()
+            return jsonify(state.to_dict()), 200
 
         elif request.method == 'DELETE':
-            for obj in state_objs.values():
-                if obj.id == state_id:
-                    storage.delete(obj)
-                    storage.save()
-                    return jsonify({}), 200
-            abort(404)
+            """Delete a specific State object"""
+            storage.delete(state)
+            storage.save()
+            return jsonify({}), 200
