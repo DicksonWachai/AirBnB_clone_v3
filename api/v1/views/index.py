cf72b3adb8adf6473742de2a6fe865759ad1a61e
DicksonWachai <wachaidickson@gmail.com>
2024-09-04 07:45:35 +0000

    Count the stats of objects storage

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,3 +10,17 @@ from flask import jsonify
 def api_status():
     """a function to return api status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def object_stats():
+    """returns the count of each object"""
+    stats = {
+            "amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("User")
+            }
+    return jsonify(stats)
