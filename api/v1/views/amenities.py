41f448dd9cecb037549679d3d32fe11ccc5ea37d
DicksonWachai <wachaidickson@gmail.com>
2024-09-04 08:30:12 +0000

    Amenity view

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,6 +1,6 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
 
-"""Handles all restful API actions for Amenity"""
+""" Handles all restful API actions for Amenity"""
 
 from api.v1.views import app_views
 from flask import request, jsonify, abort
